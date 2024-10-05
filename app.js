0775a61031fa7f7e4a89a6320230eb45564157a9
Skylar <skylar024@gmail.com>
2024-10-05 15:09:54 -0400

    Authentication Changes

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI = 'mongodb+srv://Skylar024:Laxkeeper24@mymongodbclustersrt.08hvz.mongodb.net/Node-Auth';
+mongoose.connect(dbURI)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Error: Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Error: Please enter a valide email'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Error: Please enter an password'],
+        minlength: [6, 'Error: Minimum password length is 6 characters'],
+
+    },
+});
+
+//fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created & saved', doc);
+    next();
+});
+
+//fire a function before doc saved to db
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+})
+
+//static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('Incorrect Password');
+    }
+    throw Error('Incorrect Email');
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
